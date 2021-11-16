d15a8f494cc2280bc07284edfa079b5006909429
skakulin <59971663+skakulin@users.noreply.github.com>
2021-11-16 05:22:56 +0400

    v_0.0.2

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,14 +12,14 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
               <div class="left-block d-flex">
-                <a class="navbar-brand" href="#">BookSite</a>
+                <a class="navbar-brand" href="{{ route('main') }}">BookSite</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Главная</a>
+                      <a class="nav-link active" aria-current="page" href="{{ route('main') }}">Главная</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="{{ route('add-author') }}">Добавить автора</a>
